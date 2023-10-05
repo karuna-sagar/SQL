96392e834e47d4318fb1b3983622e2d9a61669d3
Karuna sagar <karunasagar129130@gmail.com>
2023-10-05 17:12:21 +0530

    order by

--- Refining Selections.sql
+++ Refining Selections.sql
@@ -5,3 +5,13 @@ INSERT INTO books
            ('Lincoln In The Bardo', 'George', 'Saunders', 2017, 1000, 367);
 
 select * from books;
+select distinct author_fname from books;
+select distinct concat(author_fname,' ',author_lname) from books;
+select distinct author_fname,author_lname from books;
+select author_fname,author_lname from books order by author_lname desc;
+select author_fname,author_lname from books order by author_lname asc;
+select author_fname,author_lname from books order by author_fname desc;
+select author_fname,author_lname,released_year from books order by released_year;
+select author_lname,released_year from books order by 2;
+select author_lname,released_year from books order by author_lname,released_year;
+select author_lname,released_year from books order by author_lname,released_year desc;
