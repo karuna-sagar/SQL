323ed5bdfae41e444a73f698954701317fbeb13b
Karuna sagar <karunasagar129130@gmail.com>
2023-10-08 13:04:23 +0530

    Aggregate Function

--- Aggregate Function.sql
+++ Aggregate Function.sql
@@ -14,3 +14,16 @@ select concat(author_fname,' ',author_lname) as author,count(*) from books group
 select author_lname,count(*) from books  group by author_lname;
 select author_fname,author_lname,count(*),min(released_year),max(released_year) from books group by author_fname,author_lname;
 select author_lname,count(*),min(released_year),max(released_year),max(pages) from books group by author_lname;
+select sum(pages) from books;
+select author_lname,sum(pages) from books group by author_lname;
+select avg(released_year) from books;
+select avg(pages) from books;
+select released_year,avg(stock_quantity),count(*) from books group by released_year;
+
+--  EXERCISE
+select count(title) from books;
+select released_year,count(*) from books group by released_year;
+select sum(stock_quantity) from books;
+select concat(author_fname,' ',author_lname),avg(released_year) from books group by author_fname,author_lname;
+select concat(author_fname,' ' ,author_lname) from books where pages = (select max(pages) from books);
+select released_year,count(title) as '# books',avg(pages) as 'avg pages' from books group by released_year order by released_year; 
