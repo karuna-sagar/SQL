5f5207b5ce6547c860b0c0bf6ba63a59d880c8e8
Karuna sagar <karunasagar129130@gmail.com>
2023-10-12 23:45:11 +0530

    comment table created

--- INSTAGRAM CLONE.sql
+++ INSTAGRAM CLONE.sql
@@ -15,7 +15,14 @@ created_at timestamp default now(),
 foreign key(user_id) references users(id));
 
 
-
+create table comments(
+id integer auto_increment primary key,
+comment_taxt varchar(255) not null,
+user_id integer not null,
+photo_id integer not null ,
+created_at timestamp default now(),
+foreign key(user_id) references users(id),
+foreign key(photo_id) references photos(id));
 
 
 
